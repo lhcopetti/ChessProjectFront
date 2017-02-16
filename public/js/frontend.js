764d73993d82935daa3ca6dfa1141c99e5dcacd0
Luis Copetti <lhcopetti@gmail.com>
2017-02-15 23:00:56 -0200

    INT: A glimpse at the future

--- public/js/frontend.js
+++ public/js/frontend.js
@@ -1,11 +1,67 @@
 function sendCommand()
 {
     var command = $('#pgn-command').val();
+    var fenBoard = $('#fen-board').text();
 
     if (command === "") {
         alert("An empty command is not allowed!");
         return;
     }
 
-    alert("The command is: " + command);
+    var url = "https://2od867bsib.execute-api.us-west-2.amazonaws.com/prod";
+    var dataInput = {"fenBoard" : fenBoard,  "pgnCommand" : command};
+    /*
+    $.post(url, dataInput, function(dataOutput, textStatus) {
+
+        alert("Text status is: " + textStatus);
+        alert("DataOut is: " + dataOutput);
+
+
+    }, "json");
+    */
+
+    var req = createRequest();
+    req.onreadystatechange = function() { 
+        if (req.readyState != 4) return;
+
+        if (req.status != 200) {
+            alert("Erro!");
+            return;
+        }
+
+
+        var resp = req.responseText;
+        alert(resp);
+        var jsonObj = JSON.parse(resp);
+
+        alert(jsonObj.errorCode);
+
+        if (jsonObj.errorCode !== 0)
+            alert("Deu erro: " + jsonObj.errorMessage);
+        else
+        {
+            $('#fen-board').text(jsonObj.fenBoardOutput);
+        }
+    };
+    req.open("POST", url, true);
+    req.setRequestHeader("Content-type", "application/json");
+    req.send(JSON.stringify(dataInput));
+}
+
+function createRequest() {
+
+    return new XMLHttpRequest();
+/*    if (typeof xmlhttp.overrideMimeType != 'undefined') {
+      result.overrideMimeType('text/xml'); // Or anything else
+    }
+  }
+  else if (window.ActiveXObject) {
+    // MSIE
+    result = new ActiveXObject("Microsoft.XMLHTTP");
+  } 
+  else {
+    // No known mechanism -- consider aborting the application
+  }
+  return result;
+*/
 }
