5e2607c3de170522772b0aac167ad278c66270c9
Luis Copetti <lhcopetti@gmail.com>
2017-02-26 00:21:33 -0300

    INT: First available game listing

--- public/js/controller/angularController.js
+++ public/js/controller/angularController.js
@@ -1,4 +1,4 @@
-var app = angular.module('chessProjectApp', []);
+var app = angular.module('chessProjectApp', ["ngTable"]);
 
 
 app.service('loginTokenProvider', function($window)
@@ -42,14 +42,41 @@ app.controller('loginCtrl', function($scope, $http, $window, loginTokenProvider)
             }              
             //$scope.loginToken = data.token;
             //loginTokenProvider.setToken(data.token);
-            loginTokenProvider.setData({token: data.token});
+            loginTokenProvider.setData({token: data.token, login : loginInfo.name});
             $window.location.href = './mainScreen.html';
         });
 
     };
 });
 
-app.controller('listGamesCtrl', function($scope, loginTokenProvider) 
+app.controller('listGamesCtrl', 
+    [   '$scope',
+        'loginTokenProvider', 
+        'NgTableParams', 
+        '$http',
+        function($scope, loginTokenProvider, NgTableParams, $http) 
 {
+    console.log(loginTokenProvider);
     $scope.token = loginTokenProvider.getData().token;
-});
+    $scope.userLogin = loginTokenProvider.getData().login;
+
+    var self = this;
+
+
+
+    $http.get('http://localhost:3000/api/matches/user/' + $scope.userLogin, {
+        headers: {'x-access-token' : $scope.token}
+    }).success(function(data, status) 
+    {
+        if (status != 200) 
+        {
+            console.log("Erro: " + status + ". " + data.message);
+            return;
+        }              
+
+        $scope.matches = data;
+    });
+
+    $scope.matches = [ {whitePlayerID: 'fez', blackPlayerID: 'lhcopetti', matchHashID : 'alskdfjalks'} ];
+    //self.tableParams = new NgTableParams({}, { dataset: data});
+}]);
